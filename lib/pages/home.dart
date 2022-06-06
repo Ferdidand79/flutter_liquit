3744889fe1d2dfed7772c98dd42cd726dc759c68
Ferdidand79 <abelsangama@upeu.edu.pe>
2022-06-06 16:14:14 -0500

    creando al animacion liquida de las paginas

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +17,47 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("App de liquido"),
       ),
+      body: LiquidSwipe(
+        pages: [
+          Container(
+            color: Colors.black,
+            child: Center(
+              child: Text(
+                'Pagina 1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+                ),
+            ),
+          ),
+
+          Container(
+            color: Colors.blue.shade800,
+            child: Center(
+              child: Text(
+                'Pagina 2',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+                )
+            ),
+          ),
+
+          Container(
+            color: Colors.deepPurple.shade900,
+            child: Center(
+              child: Text(
+                'Pagina 3',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+                )
+            ),
+          ),
+        ],),
     );
   }
 }
